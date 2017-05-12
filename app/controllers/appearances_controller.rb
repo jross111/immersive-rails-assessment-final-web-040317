905688144aee97c02a94acb41230b12e0e190548
github username <github email address>
2017-05-12 10:47:51 -0400

    Done.

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,7 +1,9 @@
 class AppearancesController < ApplicationController
-
+  before_action :validates, only: [:create, :update]
   def new
     @appearance = Appearance.new
+    set_user
+
   end
 
   def create
@@ -13,10 +15,40 @@ class AppearancesController < ApplicationController
     end
   end
 
+  def edit
+    find_appearance_by_params
+    set_user
+  end
+
+  def update
+    find_appearance_by_params
+    @appearance.update(appearance_params)
+    redirect_to @appearance.episode
+  end
+
   private
 
+  def set_user
+    @user = User.find_by(id: session[:user_id])
+  end
+
   def appearance_params
-    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    params.require(:appearance).permit(:guest_id, :episode_id, :rating, :user_id)
+  end
+
+  def find_appearance_by_params
+    @appearance = Appearance.find_by(id: params[:id])
+  end
+
+  def valid?
+    Appearance.new(appearance_params).valid?
+  end
+
+  def validates
+    unless valid?
+      flash[:message] = "Invalid Entry"
+      render :edit
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :appearances
+
+  has_many :guests, through: :appearances
+  has_many :episodes, through: :appearances
+
+
 end
